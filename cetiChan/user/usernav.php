5dd19acb61f469e17b16bfcc89063f5fa0edc423
MEXSlacker <46721097+MEXSlacker@users.noreply.github.com>
2020-07-07 18:07:41 -0500

    Add files via upload

---
+++ cetiChan/user/usernav.php
@@ -0,0 +1,51 @@
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand pb-3" href="index.php">
+    <img src="../resources/manetologo1.png" width="30" class="d-inline-block align-top" height="30" alt="" >
+    CetiChan
+  </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="				navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">    
+      <div class="d-flex flex-row bd-highlight">
+        <div class="p-2 bd-highlight">
+          <li class="nav-item">
+            <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>'
+          </li>
+        </div>
+        <div class="p-2 bd-highlight">
+          <li class="nav-item">
+            <a class="nav-link" href="newPost.php">Crear Post</a>'
+                        
+          </li>
+        </div>
+        <!--eliminacion de la campana
+        <div class="p-2 bd-highlight">
+          <li class="nav-item col px-md-5">
+            <a href="" class="nav-link"> <i data-feather="bell"></i></a>
+          </li>
+        </div>
+        -->
+        <div class="p-2 bd-highlight">
+          <li class="nav-item col px-md-auto">
+            <!--<a href="" class="nav-link"> <i data-feather="sliders"></i></a>-->
+          </li>
+        </div>
+        <div class="p-2 bd-highlight">
+          <li class="nav-item col px-md-auto">
+            <a class="nav-link" href="logout.php">Logout</a>'
+          </li>
+        </div>
+      </div>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Buscar">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Go</button>
+    </form>
+  </div>
+</nav>
---
+++ cetiChan/user/welcome.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<?php
+		include '../libraries/libraries.php';
+
+
+	?>
+	<title></title>
+</head>
+<body>
+	<?php include 'usernav.php'; ?>
+	<h2 class="text-secondary">Welcome user</h2>
+
+</body>
+</html>
